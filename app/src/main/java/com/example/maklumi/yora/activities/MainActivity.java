a1d7b418cdb250b412382b620f7492164893d380
maklumi <maklumi@hotmail.com>
2016-02-25 10:49:41 +0800

    vid 81

--- app/src/main/java/com/example/maklumi/yora/activities/MainActivity.java
+++ app/src/main/java/com/example/maklumi/yora/activities/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.maklumi.yora.activities;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.maklumi.yora.R;
 import com.example.maklumi.yora.services.Contacts;
 import com.example.maklumi.yora.services.Messages;
 import com.example.maklumi.yora.services.entities.ContactRequest;
 import com.example.maklumi.yora.services.entities.Message;
+import com.example.maklumi.yora.services.entities.UserDetails;
 import com.example.maklumi.yora.views.MainActivityAdapter;
 import com.example.maklumi.yora.views.MainNavDrawer;
 import com.squareup.otto.Subscribe;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,6 +30,7 @@ import java.util.List;
  */
 public class MainActivity extends BaseAuthenticatedActivity implements View.OnClickListener, MainActivityAdapter.MainActivityListener {
 
+    private static final int REQUEST_SHOW_MESSAGE = 1;
     private MainActivityAdapter adapter;
     private List<Message> messages;
     private List<ContactRequest> contactRequests;
@@ -82,12 +90,50 @@ public class MainActivity extends BaseAuthenticatedActivity implements View.OnCl
 
     @Override
     public void onMessageClicked(Message message) {
-
+        Intent intent = new Intent();
+        intent.putExtra(MessageActivity.EXTRA_MESSAGE, message);
+        startActivityForResult(intent, REQUEST_SHOW_MESSAGE);
     }
 
     @Override
-    public void onContactRequestClicked(ContactRequest request, int position) {
+    public void onContactRequestClicked(final ContactRequest request, final int position) {
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_user_display, null);
+        ImageView avatar = (ImageView) dialogView.findViewById(R.id.dialog_user_display_avatar);
+        TextView displayName = (TextView) dialogView.findViewById(R.id.dialog_user_display_displayName);
 
+        UserDetails user = request.getUser();
+        displayName.setText(user.getDisplayName());
+        Picasso.with(this).load(user.getAvatarUrl()).into(avatar);
+
+        DialogInterface.OnClickListener clickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == Dialog.BUTTON_NEUTRAL){
+                    return;
+                }
+
+                boolean doAccept = which == Dialog.BUTTON_POSITIVE;
+
+                contactRequests.remove(request);
+                adapter.notifyItemRemoved(position+1);
+
+                if (contactRequests.size() == 0){
+                    adapter.notifyItemRemoved(0);
+                }
+
+                bus.post(new Contacts.RespondToContactRequestRequest(request.getUser().getId(), doAccept));
+            }
+        };
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Respond To Contact Request")
+                .setView(dialogView)
+                .setPositiveButton("Accept", clickListener)
+                .setNeutralButton("Cancel", clickListener)
+                .setNegativeButton("Reject", clickListener)
+                .setCancelable(false)
+                .create();
+        alertDialog.show();
     }
 
     @Override
@@ -132,5 +178,30 @@ public class MainActivity extends BaseAuthenticatedActivity implements View.OnCl
                 contactRequests.addAll(response.Requests);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode== REQUEST_SHOW_MESSAGE ){
+            int messageId = data.getIntExtra(MessageActivity.RESULT_EXTRA_MESSAGE_ID, -1);
+
+            if (messageId == -1) {
+                return;
+            }
+
+            for (int i = 0; i < messages.size(); i++) {
+                Message message = messages.get(i);
+                if (message.getId() == messageId) {
+                    if (resultCode == MessageActivity.REQUEST_IMAGE_DELETED) {
+                        messages.remove(message);
+                    } else {
+                        message.setIsRead(true);
+                    }
+
+                    adapter.notifyDataSetChanged();
+                    break;
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/maklumi/yora/activities/MessageActivity.java
+++ app/src/main/java/com/example/maklumi/yora/activities/MessageActivity.java
@@ -1,17 +1,269 @@
 package com.example.maklumi.yora.activities;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.maklumi.yora.R;
+import com.example.maklumi.yora.services.Messages;
+import com.example.maklumi.yora.services.entities.Message;
+import com.example.maklumi.yora.services.entities.UserDetails;
+import com.squareup.otto.Subscribe;
+
+import java.util.GregorianCalendar;
 
 /**
  * Created by Maklumi on 23-02-16.
  */
-public class MessageActivity extends BaseAuthenticatedActivity {
+public class MessageActivity extends BaseAuthenticatedActivity implements View.OnClickListener {
     public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
     public static final int REQUEST_IMAGE_DELETED = 100;
     public static final String RESULT_EXTRA_MESSAGE_ID = "RESULT_EXTRA_MESSAGE_ID";
+    public static final String EXTRA_MESSAGE_ID = "EXTRA_MESSAGE_ID";
+    public static final String STATE_MESSAGE = "STATE_MESSAGE";
+
+    private View drawer;
+    private TextView translateButton;
+    private boolean isOpen;
+    private int translateOffset;
+    private AnimatorSet currentAnimation;
+    private Message currentMessage;
+    private View progressFrame;
+    private TextView shortMessage;
+    private TextView longMessage;
 
     @Override
     protected void onYoraCreate(Bundle savedInstance) {
+        setContentView(R.layout.activity_messsage);
+
+        drawer = findViewById(R.id.activity_message_drawer);
+
+        translateButton = (TextView) findViewById(R.id.activity_message_translate);
+        progressFrame = findViewById(R.id.activity_message_progressFrame);
+        shortMessage = (TextView) findViewById(R.id.activity_message_shortMessage);
+        longMessage = (TextView) findViewById(R.id.activity_message_longMessage);
+
+        drawer.setOnClickListener(this);
+
+        shortMessage.setText("");
+        longMessage.setText("");
+        progressFrame.setVisibility(View.GONE);
+
+        toolbar.setNavigationIcon(R.drawable.ic_checkmark);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeMessage(RESULT_OK);
+            }
+        });
+
+        Message message = null;
+        if (savedInstance != null){
+            message = savedInstance.getParcelable(STATE_MESSAGE);
+        }
+
+        if (message == null){
+            message = getIntent().getParcelableExtra(EXTRA_MESSAGE);
+            if (message == null){
+                int id = getIntent().getIntExtra(EXTRA_MESSAGE_ID, -1);
+                if (id != -1 ){
+                    progressFrame.setVisibility(View.VISIBLE);
+                    bus.post(new Messages.GetMessageDetailRequest(id));
+                } else {
+                    message = new Message(
+                            1,
+                            new GregorianCalendar(),
+                            "SHORT MESSAGE",
+                            "LONG MESSAGE",
+                            null,
+                            new UserDetails(1, true, "Person","Person",""),
+                            false,
+                            false
+                    );
+                }
+            }
+        }
+
+        if (message != null){
+            showMessage(message);
+        }
+    }
+
+    private void closeMessage(int resultCode) {
+        Intent data = new Intent();
+        data.putExtra(RESULT_EXTRA_MESSAGE_ID, currentMessage.getId());
+        setResult(resultCode, data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_message_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.activity_message_menuContact){
+            Intent intent = new Intent(this, ContactActivity.class);
+            intent.putExtra(ContactActivity.EXTRA_USER_DETAILS, currentMessage.getOtherUser());
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.activity_message_menuReplay){
+            Intent intent = new Intent(this, NewMessageActivity.class);
+            intent.putExtra(NewMessageActivity.EXTRA_CONTACT, currentMessage.getOtherUser());
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.activity_message_menuDelete){
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Delete Message")
+                    .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            bus.post(new Messages.DeleteMessageRequest(currentMessage.getId()));
+                            closeMessage(REQUEST_IMAGE_DELETED);
+                        }
+                    })
+                    .setCancelable(false)
+                    .setNeutralButton("Cancel", null)
+                    .create();
+
+            dialog.show();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.activity_message_menuReplay).setVisible(
+                currentMessage!=null && !currentMessage.isFromUs());
+        return true;
+    }
+
+    @Subscribe
+    public void onMessageDetailsLoaded (final Messages.GetMessageDetailResponse response){
+        scheduler.invokeOnResume(response.getClass(), new Runnable() {
+            @Override
+            public void run() {
+                progressFrame.setVisibility(View.GONE);
+
+                if (!response.didSucceed()){
+                    response.showErrorToast(MessageActivity.this);
+                    return;
+                }
+
+                showMessage(response.Message);
+            }
+        });
+    }
+
+    private void showMessage(Message message) {
+        currentMessage = message;
+
+        String createdAt = DateUtils.formatDateTime(this, message.getCreatedAt().getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE);
+
+        if (message.isFromUs()) {
+            getSupportActionBar().setTitle("Sent on " + createdAt);
+        } else {
+            getSupportActionBar().setTitle("Received on " + createdAt);
+        }
+
+        longMessage.setText(message.getLongMessage());
+        shortMessage.setText(message.getShortMessage());
+
+        if (message.getImageUrl() != null && !message.getImageUrl().isEmpty()) {
+            //TODO loadn image
+        }
+
+        invalidateOptionsMenu();
+        Intent defaultResult = new Intent();
+        defaultResult.putExtra(RESULT_EXTRA_MESSAGE_ID, message.getId());
+        setResult(RESULT_OK, defaultResult);
+
+        if (!message.isRead())
+        bus.post(new Messages.MarkMessageAsReadRequest(message.getId()));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(STATE_MESSAGE, currentMessage);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+            translateOffset = drawer.getHeight() - translateButton.getHeight();
+            drawer.setTranslationY(translateOffset);
 
     }
+
+    @Override
+    public void onClick(View v) {
+        isOpen = !isOpen;
+
+        if (currentAnimation != null){
+            currentAnimation.cancel();
+        }
+
+        int currentBackgroundColor = ((ColorDrawable ) drawer.getBackground()).getColor();
+        int translationY, color;
+
+        if (!isOpen){
+            translationY = 0;
+            color = Color.parseColor("#EE1990FC");
+            translateButton.setText("Close");
+
+        } else {
+            translationY = translateOffset;
+            color = Color.parseColor("#221990FC");
+            translateButton.setText("Translate");
+        }
+
+        ObjectAnimator translateAnimator = ObjectAnimator
+                .ofFloat(drawer, "translationY", translationY)
+                .setDuration(100);
+
+        ObjectAnimator colorAnimator = ObjectAnimator
+                .ofInt(drawer, "backgroundColor", currentBackgroundColor, color)
+                .setDuration(100);
+
+        colorAnimator.setEvaluator(new ArgbEvaluator());
+
+        currentAnimation = new AnimatorSet();
+        currentAnimation.setDuration(400);
+        currentAnimation.play(translateAnimator).with(colorAnimator);
+        currentAnimation.start();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
